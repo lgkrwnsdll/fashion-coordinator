2192e0ad3b7f0a629bb68a59428eb4e18f045479
hakjoon <6210182@hanmail.net>
2025-04-02 01:51:34 +0900

    project setup

---
+++ src/main/java/com/example/fashioncoordinator/FashionCoordinatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.fashioncoordinator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FashionCoordinatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FashionCoordinatorApplication.class, args);
+    }
+
+}
