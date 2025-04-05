219e9fd768f7e0b6db083675b1b9de9652501116
hakjoon <6210182@hanmail.net>
2025-04-06 01:38:39 +0900

    Refactor: admin 모듈 서비스 계층 역방향 참조 해소

--- src/main/java/com/example/fashioncoordinator/admin/domain/AdminProductService.java
+++ src/main/java/com/example/fashioncoordinator/admin/domain/AdminProductService.java
@@ -1,6 +1,5 @@
 package com.example.fashioncoordinator.admin.domain;
 
-import com.example.fashioncoordinator.admin.api.ManageProductRequestDto;
 import com.example.fashioncoordinator.db.ProductEntity;
 import com.example.fashioncoordinator.db.ProductJpaRepository;
 import lombok.RequiredArgsConstructor;
@@ -14,23 +13,23 @@ public class AdminProductService {
     private final ProductJpaRepository productJpaRepository;
 
     @Transactional
-    public void manageProduct(ManageProductRequestDto requestDto) {
-        ProductEntity productEntity = productJpaRepository.findByBrand(requestDto.getBrand())
+    public void manageProduct(String brand, BrandPriceInfo brandPriceInfo) {
+        ProductEntity productEntity = productJpaRepository.findByBrand(brand)
             .orElse(null);
 
         if (productEntity != null) {
-            productEntity.updateAllCategory(requestDto.toPriceInfo());
+            productEntity.updateAllCategory(brandPriceInfo);
         } else {
             ProductEntity newProductEntity = ProductEntity.builder()
-                .brand(requestDto.getBrand())
-                .tops(requestDto.getTops())
-                .outerwear(requestDto.getOuterwear())
-                .pants(requestDto.getPants())
-                .sneakers(requestDto.getSneakers())
-                .bag(requestDto.getBag())
-                .hat(requestDto.getHat())
-                .socks(requestDto.getSocks())
-                .accessories(requestDto.getAccessories())
+                .brand(brand)
+                .tops(brandPriceInfo.getTops())
+                .outerwear(brandPriceInfo.getOuterwear())
+                .pants(brandPriceInfo.getPants())
+                .sneakers(brandPriceInfo.getSneakers())
+                .bag(brandPriceInfo.getBag())
+                .hat(brandPriceInfo.getHat())
+                .socks(brandPriceInfo.getSocks())
+                .accessories(brandPriceInfo.getAccessories())
                 .build();
 
             productJpaRepository.save(newProductEntity);
